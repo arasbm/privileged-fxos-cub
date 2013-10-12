debdf04e66629ed3a3a93a0e3e53a219b4523a0a
Aras Balali Moghaddam <arasbm@gmail.com>
2013-10-11 17:30:19 -0700

    trying to get brick to load with alameda

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
-define(function() {
+define(function(require) {
+
+var brick = require('helper/brick');
 
 if (document.readyState == 'complete') {
   init();
 } else {
-    document.addEventListener('DOMContentLoaded', init);
+  document.addEventListener('DOMContentLoaded', init);
 }
 
 function init() {
